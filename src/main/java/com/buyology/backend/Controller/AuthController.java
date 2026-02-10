9627c0e17fe178083af9827e09ca89e63726947a
SaiPavanN <saipavannagandla007@gmail.com>
2026-02-10 00:45:56 -0600

    chore: un-used import

--- src/main/java/com/buyology/backend/Controller/AuthController.java
+++ src/main/java/com/buyology/backend/Controller/AuthController.java
@@ -18,7 +18,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
