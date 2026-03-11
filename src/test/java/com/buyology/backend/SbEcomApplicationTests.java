191f62c681fc1cd2ca1631e0b07a9ab1532e192b
SaiPavanN <saipavannagandla007@gmail.com>
2026-03-11 01:03:06 -0500

    fix: pipeline

--- src/test/java/com/buyology/backend/SbEcomApplicationTests.java
+++ src/test/java/com/buyology/backend/SbEcomApplicationTests.java
@@ -1,9 +1,12 @@
 package com.buyology.backend;
 
+import com.buyology.backend.config.InfisicalPropertySource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(initializers = InfisicalPropertySource.class)
 class SbEcomApplicationTests {
 
 	@Test
